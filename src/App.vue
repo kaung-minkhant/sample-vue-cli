894dfcdd6fe51f2cad3f732613a196089779f773
Kaung Min Khant <kaungminkhant.official@gmail.com>
2023-09-14 16:10:52 +0630

    cli template refs, multiple components, props, custom events, slots, teleport

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <teleport to="#modals" v-if="showModal">
+    <Modal theme="" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">Hello</a>
+        <a href="#">World</a>
+      </template>
+      <h1>Hello World</h1>
+      <p>This is the hello world text</p>
+    </Modal>
+  </teleport>
+  <button @click.alt="toggleModal">Show Modal (alt)</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Modal,
+  },
+  data() {
+    return {
+      title: "My First Vue App :-)",
+      header: "My Modal",
+      text: "this is modal body",
+      showModal: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
+#app, #modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/components/Modal.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{sale: theme==='sale'}">
+            <slot>This is default</slot>
+
+            <div class="links">
+                <slot name="links"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['theme'],
+    methods: {
+        closeModal() {
+            this.$emit('close')
+        }
+    }
+}
+</script>
+
+<style>
+.modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background-color: white;
+    border-radius: 10px;
+}
+
+.modal.sale {
+    background-color: crimson;
+    color: white;
+}
+
+.modal.sale h1 {
+    color: white
+}
+
+.modal .links {
+    text-align: center;
+    margin: 30px 0 10px 0;
+}
+
+.modal .links a {
+    color: #333;
+    padding: 8px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
+}
+.backdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-color: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+}
+
+p {
+    font-style: normal;
+}
+</style>
